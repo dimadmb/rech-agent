3c2855944e5e176dacad2c8026126dabe55e5f41
Dmitry_K <12nas24@mail.ru>
2016-04-20 13:34:15 +0300

    добавили кнопку купить онлайн и настроили отправку почты

--- src/BaseBundle/Controller/OrderController.php
+++ src/BaseBundle/Controller/OrderController.php
@@ -32,27 +32,31 @@ class OrderController extends Controller
 				->add('name','text',array('label'=>'Фамилия, Имя, Отчество'))
 				->add('phone','text',array('label'=>'Контактный телефон'))
 				->add('button', 'submit',array('label' => 'Отправить заявку'))
-				->add('cruisecode','hidden',array('data'=>$code))
+				//->add('cruisecode','hidden',array('data'=>$code))
 				->getForm()
 		;
-/*
+
 		if ($request->getMethod() == 'POST') {  
 			$form->handleRequest($request);
+			
+
+			
 			
 			// отправим тут письмо
 			
 			//$order = new CruiseOrder();
 
-			
+			/*
 			$em = $this->getDoctrine()->getManager();
 			$em->persist($order);
 			$em->flush();
-			
-		}*/
+			*/
+		}
 		
 		
         if ($form->isSubmitted() && $form->isValid()) {
-            $order
+            /* 
+		    $order
 				->setCruisecode($form['code'])
 				->setName($form['name'])
 				->setPhone($form['phone'])
@@ -61,31 +65,47 @@ class OrderController extends Controller
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($order);
             $entityManager->flush();
-
+			*/
+			
             // Flash messages are used to notify the user about the result of the
             // actions. They are deleted automatically from the session as soon
             // as they are accessed.
             // See http://symfony.com/doc/current/book/controller.html#flash-messages
             $this->addFlash('success', 'post.created_successfully');
+			
+			
+			$mailer = $this->get('mailer');
+			
+
+			
+			
+			$body = $form->getData()->getName(). '  '.$form->getData()->getPhone() ;
+			
+			$message = \Swift_Message::newInstance()
+				->setSubject('Заказ')
+				->setFrom(array('test-rech-agent@yandex.ru'=>'Речное агентство'))
+				->setTo('dkochetkov@vodohod.ru')
+				->setBody($body)
+			;
+			$mailer->send($message);
+
+			
         }		
 
 		$cruise = null;
 
 		if($code != null)
 		{
-			
 			$repository = $this->getDoctrine()->getRepository('BaseBundle:CruiseCruise');
 			$cruise = $repository->findOneByCode($code);
 	
 		};
 		
-		
-		
-		
+
+		//$form->getForm(); 
+
 
 		
-		//$form->getForm(); 
-		
-		return array('page' => $page,'form'=>$form->createView(),'cruise'=>$cruise);
+		return array('page' => $page,'form'=>$form->createView(),'cruise'=>$cruise,);
 	}
 }

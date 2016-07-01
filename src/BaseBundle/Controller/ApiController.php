612f9549f8ca01a098a80f4890def76fc4d495cf
Dmitry_K <12nas24@mail.ru>
2016-07-01 12:53:38 +0300

    Информация о продажах в выгрузке для агентств

--- src/BaseBundle/Controller/ApiController.php
+++ src/BaseBundle/Controller/ApiController.php
@@ -59,13 +59,39 @@ class ApiController extends Controller
 		$statement = $connection->prepare($sql);
 		$statement->execute();
 		$results = $statement->fetchAll();
+
+		
+		$sql="
+		SELECT * 
+		FROM aa_schet 
+
+		LEFT JOIN aa_order ON aa_order.id_schet = aa_schet.id
+
+		WHERE id_tur = $cruise_code
+
+		AND aa_order.is_delete = 0
+
+		AND aa_schet.status = 1
+		";
+
+		$statement = $connection->prepare($sql);
+		$statement->execute();
+		$results = $statement->fetchAll();
+
+		
 		// нужно получить активные каюты
-		$active_rooms = array();
+		$active_rooms = array();	
 		foreach($results as $item)
 		{
 			$active_rooms[] = $item['num'];
 		}
-	
+		
+		// нужно получить занятые каюты
+		$no_available_rooms = array();		
+		foreach($results as $item)
+		{
+			$no_available_rooms[] = $item['num'];
+		}
 	
 		$cruisesRepository = $this->getDoctrine()->getRepository('BaseBundle:CruiseCruise');
 			
@@ -112,14 +138,25 @@ class ApiController extends Controller
 			$dump[] = $price;
 			foreach($price->getCabin()->getRooms() as $room)
 			{
+				$is_happy_kayuta = 0;
+				$is_special_kayuta = 0;
+				$available_rooms = 1;
 				
 				if(in_array( $room->getRoomNumber(), $active_rooms))
 				{
 					$price_koeff = (int)( $price->getPrice() * $koeff);
+					$is_happy_kayuta = $cruise_prices->getCode()->getBurningCruise();
+					$is_special_kayuta = $cruise_prices->getCode()->getSpecialOffer();
+					
 				}
 				else 
 				{
 					$price_koeff = $price->getPrice();
+				}
+				
+				if(in_array( $room->getRoomNumber(), $no_available_rooms))
+				{
+					$available_rooms = 0;
 				}
 				
 				$rooms[$room->getRoomNumber()][$price->getRpId()->getRpId()][$price->getTariff()->getId()] = array(
@@ -133,6 +170,9 @@ class ApiController extends Controller
 						'tariff_id' => $price->getTariff()->getId(),
 						'price_old' =>$price->getPrice(),
 						'price' => $price_koeff,
+						'is_happy' => $is_happy_kayuta,
+						'is_special' => $is_special_kayuta,
+						'available_rooms' => $available_rooms,
 						);
 			}
 			
